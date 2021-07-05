225fd618682d4f52a3bc258e5dafa2e06a0430a3
DocLock17 <47020392+DocLock17@users.noreply.github.com>
2021-07-05 16:48:43 -0500

    Update installSuite.sh

--- installSuite.sh
+++ installSuite.sh
@@ -1,3 +1,19 @@
+#!/usr/bin/env bash
+
+update_selected() {
+	echo " "
+	echo "Updating . . ."
+	echo " "
+	sudo apt update
+	sudo apt-get update
+	sudo apt dist-upgrade -y --allow-downgrades
+	sudo apt-get dist-upgrade -y --allow-downgrades
+	sudo apt autoremove -y
+	sudo apt-get autoremove -y
+	echo "System Updated"
+	echo " "
+}
+
 install_lambda_stack() {
   LAMBDA_REPO=$(mktemp) && \
   wget -O${LAMBDA_REPO} https://lambdalabs.com/static/misc/lambda-stack-repo.deb && \
